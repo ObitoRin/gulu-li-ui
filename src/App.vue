3afaa476f515c8f13caca08148b869b2d46a14a0
ObitoRin <2209246794@qq.com>
2022-04-29 00:37:06 +0800

    搭建html结构

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <div>
-    导航栏 | <router-link to="/">首页</router-link> |
-    <router-link to="/xxx">Rin2</router-link>
-  </div>
   <!-- 在App.vue中展示路由对应的组件 -->
   <router-view />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,16 @@ import App from './App.vue';
 import './index.css';
 import { createWebHashHistory, createRouter } from 'vue-router';
 // createMemoryHistory 内存型路由 、createWebHashHistory Hash型路由、createWebHistory History型路由
-import Rin from './components/Rin.vue';
-import Rin2 from './components/Rin2.vue';
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
 
 // 路由模式
 const history = createWebHashHistory();
 
 // 配置路由 (根据path展示对应的组件)
 const routes = [
-  { path: '/', component: Rin },
-  { path: '/xxx', component: Rin2 }
+  { path: '/', component: Home },
+  { path: '/doc', component: Doc }
 ];
 
 // 创建路由
