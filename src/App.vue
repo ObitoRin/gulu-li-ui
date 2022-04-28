3230051360a6ab8b9636f31eb255d80cc69b61a0
ObitoRin <2209246794@qq.com>
2022-04-28 23:31:11 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Rin />
+</template>
+
+<script>
+import Rin from './components/Rin.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Rin
+  }
+};
+</script>
