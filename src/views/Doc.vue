06c8088a5030ea7505a181139780b7f5f94e466c
ObitoRin <2209246794@qq.com>
2022-05-01 02:02:58 +0800

    整体布局 css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,27 +1,29 @@
 <template>
   <div>
-    <Topnav />
-    <div class="content">
-      <aside v-if="menuVisible">
-        <h2>组件列表</h2>
-        <ol>
-          <li>
-            <router-link to="doc/switch">Switch 组件</router-link>
-          </li>
-          <li>
-            <router-link to="doc/button">Button 组件</router-link>
-          </li>
-          <li>
-            <router-link to="doc/dialog">Dialog 组件</router-link>
-          </li>
-          <li>
-            <router-link to="doc/tabs">Tabs 组件</router-link>
-          </li>
-        </ol>
-      </aside>
-      <main>
-        <router-view></router-view>
-      </main>
+    <div class="layout">
+      <Topnav class="nav" />
+      <div class="content">
+        <aside v-if="menuVisible">
+          <h2>组件列表</h2>
+          <ol>
+            <li>
+              <router-link to="doc/switch">Switch 组件</router-link>
+            </li>
+            <li>
+              <router-link to="doc/button">Button 组件</router-link>
+            </li>
+            <li>
+              <router-link to="doc/dialog">Dialog 组件</router-link>
+            </li>
+            <li>
+              <router-link to="doc/tabs">Tabs 组件</router-link>
+            </li>
+          </ol>
+        </aside>
+        <main>
+          <router-view></router-view>
+        </main>
+      </div>
     </div>
   </div>
 </template>
@@ -40,6 +42,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
+    }
+  }
+}
+.content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
 aside {
   background: lightblue;
   width: 150px;
@@ -48,6 +77,8 @@ aside {
   top: 0;
   left: 0;
   padding-top: 70px;
+  height: 100%;
+  padding-top: 70px;
   > h2 {
     margin-bottom: 4px;
   }
@@ -55,6 +86,9 @@ aside {
     > li {
       padding: 4px 0;
     }
+  }
+  main {
+    overflow: auto;
   }
 }
 </style>
