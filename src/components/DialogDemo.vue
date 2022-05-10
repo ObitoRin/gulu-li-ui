5e677c2344207049748ae3f78ef67b0832efe802
ObitoRin <2209246794@qq.com>
2022-05-10 22:14:03 +0800

    使用 Teleport

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,21 +1,32 @@
 <template>
   <div>Dialog 示例</div>
-  <Button @click="toggle">toggle</Button>
-  <Dialog
-    title="标题"
-    v-model:visible="visible"
-    :onClickOverlay="false"
-    :ok="ok"
-    :cancel="cancel"
-  >
-    <template v-slot:title>
-      <strong>提示</strong>
-    </template>
-    <template v-slot:content>
-      <div>hi</div>
-      <div>这是一段信息</div>
-    </template>
-  </Dialog>
+  <div style="position: relative; z-index: 1">
+    <Button @click="toggle">toggle</Button>
+    <Dialog
+      title="标题"
+      v-model:visible="visible"
+      :onClickOverlay="false"
+      :ok="ok"
+      :cancel="cancel"
+    >
+      <template v-slot:title>
+        <strong>提示</strong>
+      </template>
+      <template v-slot:content>
+        <div>hi</div>
+        <div>这是一段信息</div>
+      </template>
+    </Dialog>
+  </div>
+  <div
+    style="
+      position: relative;
+      z-index: 2;
+      width: 300px;
+      height: 300px;
+      background: red;
+    "
+  ></div>
 </template>
 <script lang="ts">
 import { ref } from 'vue';
