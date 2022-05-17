3c03b3ec8ccc4ec2731969296fb91fc7178267ac
ObitoRin <2209246794@qq.com>
2022-05-18 00:28:58 +0800

    更新 SwitchDemo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,17 +1,39 @@
 <template>
   <div>
-    <!-- 自动监听 value 的变化 -->
-    <Switch v-model:value="bool" :disabled="disabled" />
-
-    <!-- <Switch :value="bool" @update:value="bool = $event" /> -->
-    <!-- $event 是 emit 传的值 -->
+    <h1>Switch 组件示例</h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <Switch v-model:value="bool" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+      </div>
+    </div>
+    <div class="demo">
+      <h2>支持 disabled</h2>
+      <div class="demo-component">
+        <Switch v-model:value="bool" disabled />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { ref } from 'vue';
 import Switch from '../lib/Switch.vue';
+import Button from '../lib/Button.vue';
+
 export default {
-  components: { Switch },
+  components: { Switch, Button },
   setup() {
     const bool = ref(false);
     const disabled = ref(false);
@@ -19,3 +41,31 @@ export default {
   }
 };
 </script>
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  > h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    > pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
+</style>
